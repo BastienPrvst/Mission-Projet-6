9c31bdaf38e2af16a2e3b9c4b416ad6746716cc2
Spirit <a@a.a>
2024-07-01 18:31:58 +0200

    Create database book and user + getter/setters

---
+++ src/entity/Book.php
@@ -0,0 +1,95 @@
+<?php
+
+class Book
+{
+
+    private int  $id;
+    private int $userId;
+    private string $title;
+    private string $description;
+    private string $author;
+    private string $image;
+    private bool $statut;
+
+    public function __construct(int $id, int $userId, string $title, string $description, string $author, string $image){
+        $this->id = $id;
+        $this->userId = $userId;
+        $this->title = $title;
+        $this->description = $description;
+        $this->author = $author;
+        $this->image = $image;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): void
+    {
+        $this->author = $author;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+
+    public function isStatut(): bool
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(bool $statut): void
+    {
+        $this->statut = $statut;
+    }
+
+
+
+}
---
+++ src/entity/User.php
@@ -0,0 +1,76 @@
+<?php
+
+class User
+
+{
+    private int $id;
+    private string $pseudo;
+    private string $password;
+    private string $email;
+    private string $avatar;
+
+    public function __construct(int $id, string $pseudo, string $password, string $email){
+        $this->id = $id;
+        $this->pseudo = $pseudo;
+        $this->password = $password;
+        $this->email = $email;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPseudo(): string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): void
+    {
+        $this->pseudo = $pseudo;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getAvatar(): string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(string $avatar): void
+    {
+        $this->avatar = $avatar;
+    }
+
+}
